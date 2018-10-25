04b818f8e3b1dbcc788c8b72465282de5e9acb2f
Neal Bowser <rh1thmmomk@gmail.com>
2018-10-25 19:58:03 -0400

    yarn out

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ module.exports = function (passport) {
 	
 
 	// If no API routes are hit, send the React app
-	router.use(function(req, res) {
-	  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-	});
+	// router.use(function(req, res) {
+	//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+	// });
 
 	return router;
 };
